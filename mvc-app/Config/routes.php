d5e238b6bf83dae1d0fe11bce2ac6836d4b69996
Andrii Popov <andrew.p@codesushi.co>
2017-05-15 17:57:02 +0000

    add controller

--- mvc-app/Config/routes.php
+++ mvc-app/Config/routes.php
@@ -7,6 +7,6 @@ return  array(
     'default' => new Route('/', 'Default', 'index'),
     'index' => new Route('/index.php', 'Default', 'index'),
     'books_list' => new Route('/books', 'Book', 'index'),
-    'book_page' => new Route('/book-{id}\.html', 'Book', 'show', array('id' => '[0-9]+') ),
+    'books_page' => new Route('/book-{id}\.html', 'Book', 'show', array('id' => '[0-9]+') ),
     'feedback_page' => new Route('/feedback', 'Default', 'feedback'),
 );
--- mvc-app/webroot/index.php
+++ mvc-app/webroot/index.php
@@ -15,31 +15,40 @@ spl_autoload_register(function($className) {
     require_once $file;
 });
 
-\Library\Session::start();
+try {
+    \Library\Session::start();
 
-$request = new \Library\Request();
-
-$router = new \Library\Router(ROOT . 'Config' . DS . 'routes.php');
-$router->match($request);
-
-
-$pdo = new \PDO('mysql: host=localhost; dbname=mvc', 'root', '');
-$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-
-$container = new \Library\Container();
-$container->set('router', $router);
-$container->set('db_connection', $pdo);
-$container->set('repository', (new \Library\RepositoryManager())->setPdo($pdo));
-
-$route = $router->getCurrentRoute();
-
-$controller = 'Controller\\' . $route->controller . 'Controller';
-$action = $route->action . 'Action';
-
-$controller = (new $controller())->setContainer($container); // Controller\DefaultController
-
-if (!method_exists($controller, $action)) {
-    throw new Exception("{$action} not found");
+    $request = new \Library\Request();
+    
+    $router = new \Library\Router(ROOT . 'Config' . DS . 'routes.php');
+    
+    
+    
+    $pdo = new \PDO('mysql: host=localhost; dbname=mvc', 'root', '');
+    $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    
+    $container = new \Library\Container();
+    $container->set('router', $router);
+    $container->set('db_connection', $pdo);
+    $container->set('repository', (new \Library\RepositoryManager())->setPdo($pdo));
+    
+    $router->match($request);
+    
+    $route = $router->getCurrentRoute();
+    
+    $controller = 'Controller\\' . $route->controller . 'Controller';
+    $action = $route->action . 'Action';
+    
+    $controller = (new $controller())->setContainer($container); // Controller\DefaultController
+    
+    if (!method_exists($controller, $action)) {
+        throw new Exception("{$action} not found");
+    }
+    
+    echo $controller->$action($request);
+    
+} catch (\Exception $e) {
+    $controller = (new \Controller\ExceptionController)->setContainer($container);
+    echo $controller->handleAction($request, $e);
 }
 
-echo $controller->$action($request);
